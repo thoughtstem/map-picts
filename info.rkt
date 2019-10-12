4528d85e38ee7b408ba899c0a27560844954e626
Stephen <stephen@thoughtstem.com>
2019-10-12 08:18:08 -0700

    fix dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "map-picts")
-(define deps '("base"))
+(define deps '("base" "map-widget"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/map-picts.scrbl" ())))
 (define pkg-desc "Description Here")
