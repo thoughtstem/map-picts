eca003ece01dd47146097989899ba570740241bf
Stephen <stephen@thoughtstem.com>
2019-10-15 07:44:21 -0700

    track feature, and tried to improve the wait strategy

--- map-pict.rkt
+++ map-pict.rkt
@@ -4,6 +4,7 @@
 
 (require pict
          map-widget/private/map-impl
+         map-widget/private/map-tiles
          racket/draw
          "./util.rkt"
          )
@@ -13,10 +14,19 @@
 (define (map-pict width height
                   #:zoom [zoom 9]
                   #:position [position #f]
-                  . markers)
+                  . markers-and-tracks)
+  (set! position
+    (if (marker? position)
+      (marker-position position)
+      position))
+
+
   (define renderer (new map-impl%
                         [width width] [height height]
                         [zoom zoom] [position position]))
+
+  (define markers (filter-not list? markers-and-tracks))
+  (define tracks (filter list? markers-and-tracks))
 
 
   (for ([m markers])
@@ -25,14 +35,27 @@
           (marker-text m)
           1
           (marker-color m)))
-  
 
+  
+  (for ([t tracks])
+    (define l (map marker-position t))
+    (send renderer add-track
+          l
+          #f))
+
+  (displayln (get-download-backlog))
   
   (dc (lambda (dc x y)
         (send renderer draw dc x y))
       width height)
 
   (sleep (sleep-time))
+
+  (let loop () 
+    (when (> (get-download-backlog) 0)
+      (displayln (~a "Map tiles to fetch: " (get-download-backlog))) 
+      (sleep 1)
+      (loop)))
 
   (dc (lambda (dc x y)
         (send renderer draw dc x y))
--- scribblings/map-picts.scrbl
+++ scribblings/map-picts.scrbl
@@ -7,4 +7,44 @@
 
 @defmodule[map-picts]
 
-Package Description Here
+
+@defproc[(map-pict [#:zoom zoom number?] [#:position center-pos (or/c lat-lon? marker?)] [markers-or-lists (or/c marker? (listof marker?))] ...) pict?]{
+
+@codeblock{
+(define lafayette
+  (marker #(30.230188 -92.010391) 
+          "Born"
+          (color 255 0 0)))
+
+(define riyadh
+  (marker #(24.687246 46.752999) 
+          "Raised"
+          (color 255 0 0)))
+
+(define atlantic1
+  (marker
+   #(44.930148 -21.643282)
+   "Ocean"
+   (color 0 0 255)))
+
+(define atlantic2
+  (marker
+   #(44.930148 -1.643282)
+   "Ocean"
+   (color 0 0 255)))
+
+(map-pict 300 300
+          #:position
+          atlantic1
+          #:zoom 1
+          lafayette
+          riyadh
+          (list
+           lafayette
+           atlantic2
+           riyadh
+           atlantic1
+           lafayette))
+}
+
+}
